df6330dbd39d4d8b420625c3d923677d193d3c3d
KevR98 <ramil.kevin98@gmail.com>
2025-09-01 13:56:53 +0200

    nothing

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,9 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import FooterHome from './components/Footer/FooterHome';
-import FooterProfile from './components/Footer/FooterProfile';
 
 function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<FooterHome />} />
-          <Route path='/profile' element={<FooterProfile />} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
