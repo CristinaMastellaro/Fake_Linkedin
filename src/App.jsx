9e86c16962672f267394261bd1b74171a4496e20
KevR98 <ramil.kevin98@gmail.com>
2025-09-01 13:53:30 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import MyFooter from './components/Footer/MyFooter';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import FooterHome from './components/Footer/FooterHome';
 import FooterProfile from './components/Footer/FooterProfile';
@@ -14,7 +13,6 @@ function App() {
           <Route path='/' element={<FooterHome />} />
           <Route path='/profile' element={<FooterProfile />} />
         </Routes>
-        <MyFooter />
       </BrowserRouter>
     </>
   );
--- src/components/Footer/FooterHome.jsx
+++ src/components/Footer/FooterHome.jsx
@@ -1,4 +1,4 @@
-import { Col, Row } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 
 function FooterHome() {
   /*   const text = [
