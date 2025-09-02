1566f9b02ed27f95ee0e240e693113429baba81a
PernaGiuseppe <giuseppeperna97@hotmail.com>
2025-09-02 15:38:13 +0200

    up

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,66 @@
-import { Navbar, NavDropdown, Nav, Form, FormControl, Container, Row, Col } from "react-bootstrap";
-import { HouseFill, PeopleFill, BriefcaseFill, ChatDotsFill, BellFill, PersonCircle, CaretDownFill, Grid3x3GapFill, StarFill } from "react-bootstrap-icons";
-import "../css/Navbar.css";
-import { useState } from "react";
-import  { useLocation, Link, } from 'react-router-dom'
+import {
+  Navbar,
+  NavDropdown,
+  Nav,
+  Form,
+  FormControl,
+  Container,
+  Row,
+  Col,
+} from 'react-bootstrap'
+import {
+  HouseFill,
+  PeopleFill,
+  BriefcaseFill,
+  ChatDotsFill,
+  BellFill,
+  PersonCircle,
+  CaretDownFill,
+  Grid3x3GapFill,
+  StarFill,
+} from 'react-bootstrap-icons'
+import '../css/Navbar.css'
+import { useState } from 'react'
+import { useLocation, Link } from 'react-router-dom'
 
 export default function CustomNavbar() {
-  const [addFlex, setAddFlex] = useState(false);
-  const location = useLocation();
+  const [addFlex, setAddFlex] = useState(false)
+  const location = useLocation()
 
   return (
-    <Navbar bg="light" expand="md" className="border-bottom fixed-top px-3 py-0">
-      <Container fluid className="d-flex align-items-center justify-content-between px-5">
+    <Navbar
+      bg="light"
+      expand="lg"
+      className="border-bottom fixed-top px-3 py-0"
+    >
+      <Container
+        fluid
+        className="d-flex align-items-center justify-content-between px-5"
+      >
         {/* Logo + Search sempre visibili */}
         <div className="d-flex align-items-center gap-3">
           {/* Logo */}
-          <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 24 24">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="35"
+            height="35"
+            viewBox="0 0 24 24"
+          >
             <rect width="24" height="24" fill="#0A66C2" rx="2" />
-            <path fill="#FFFFFF" d="M6.94 19H3.28V9h3.66v10zM5.11 7.41c-1.18 0-2.13-.96-2.13-2.13S3.93 3.15 5.11 3.15s2.13.96 2.13 2.13-.95 2.13-2.13 2.13zm13.79 11.59h-3.66v-4.83c0-1.15-.41-1.94-1.44-1.94-.78 0-1.24.53-1.44 1.05-.07.17-.09.41-.09.64v5.08h-3.66s.05-8.24 0-9.09h3.66v1.29c.49-.76 1.37-1.84 3.33-1.84 2.43 0 4.26 1.59 4.26 5.01v4.63z" />
+            <path
+              fill="#FFFFFF"
+              d="M6.94 19H3.28V9h3.66v10zM5.11 7.41c-1.18 0-2.13-.96-2.13-2.13S3.93 3.15 5.11 3.15s2.13.96 2.13 2.13-.95 2.13-2.13 2.13zm13.79 11.59h-3.66v-4.83c0-1.15-.41-1.94-1.44-1.94-.78 0-1.24.53-1.44 1.05-.07.17-.09.41-.09.64v5.08h-3.66s.05-8.24 0-9.09h3.66v1.29c.49-.76 1.37-1.84 3.33-1.84 2.43 0 4.26 1.59 4.26 5.01v4.63z"
+            />
           </svg>
 
           {/* Barra di ricerca */}
           <Form className="search-form">
             <i className="bi bi-search search-icon"></i>
-            <FormControl type="search" placeholder="Cerca" className="px-5 search-input rounded-5" />
+            <FormControl
+              type="search"
+              placeholder="Cerca"
+              className="px-5 search-input rounded-5"
+            />
           </Form>
         </div>
 
@@ -33,35 +71,75 @@ export default function CustomNavbar() {
           {/* Griglia di navigazione */}
           <Row className="g-3 text-center ms-auto text-center d-flex flex-sm-row align-items-sm-center">
             <Col xs={4} md="auto">
-              <Nav.Link className={'nav-link' + (location.pathname === '/' ? ' active' : '') + " d-flex flex-column align-items-center"} to="/" as={Link}>
+              <Nav.Link
+                className={
+                  'nav-link' +
+                  (location.pathname === '/' ? ' active' : '') +
+                  ' d-flex flex-column align-items-center'
+                }
+                to="/"
+                as={Link}
+              >
                 <HouseFill size={22} />
                 <span className="small">Home</span>
               </Nav.Link>
             </Col>
 
             <Col xs={4} md="auto">
-              <Nav.Link className={'nav-link' + (location.pathname === '/rete' ? ' active' : '') + " d-flex flex-column align-items-center"} to="/rete" as={Link}>
+              <Nav.Link
+                className={
+                  'nav-link' +
+                  (location.pathname === '/rete' ? ' active' : '') +
+                  ' d-flex flex-column align-items-center'
+                }
+                to="/rete"
+                as={Link}
+              >
                 <PeopleFill size={22} />
                 <span className="small">Rete</span>
               </Nav.Link>
             </Col>
 
             <Col xs={4} md="auto">
-              <Nav.Link className={'nav-link' + (location.pathname === '/lavoro' ? ' active' : '') + " d-flex flex-column align-items-center"} to="/lavoro" as={Link}>
+              <Nav.Link
+                className={
+                  'nav-link' +
+                  (location.pathname === '/lavoro' ? ' active' : '') +
+                  ' d-flex flex-column align-items-center'
+                }
+                to="/lavoro"
+                as={Link}
+              >
                 <BriefcaseFill size={22} />
                 <span className="small">Lavoro</span>
               </Nav.Link>
             </Col>
 
             <Col xs={4} md="auto">
-              <Nav.Link className={'nav-link' + (location.pathname === '/lavoro' ? ' active' : '') + " d-flex flex-column align-items-center"} to="/lavoro" as={Link}>
+              <Nav.Link
+                className={
+                  'nav-link' +
+                  (location.pathname === '/lavoro' ? ' active' : '') +
+                  ' d-flex flex-column align-items-center'
+                }
+                to="/lavoro"
+                as={Link}
+              >
                 <ChatDotsFill size={22} />
                 <span className="small">Messaggi</span>
               </Nav.Link>
             </Col>
 
             <Col xs={4} md="auto">
-              <Nav.Link className={'nav-link' + (location.pathname === '/notifiche' ? ' active' : '') + " d-flex flex-column align-items-center"} to="/notifiche" as={Link}>
+              <Nav.Link
+                className={
+                  'nav-link' +
+                  (location.pathname === '/notifiche' ? ' active' : '') +
+                  ' d-flex flex-column align-items-center'
+                }
+                to="/notifiche"
+                as={Link}
+              >
                 <BellFill size={22} />
                 <span className="small">Notifiche</span>
               </Nav.Link>
@@ -95,24 +173,71 @@ export default function CustomNavbar() {
                     <div className="user-actions px-2">
                       <Link to="/profile">
                         <button className="btn border border-primary text-primary rounded-pill me-1">
-                            Visualizza profilo
+                          Visualizza profilo
                         </button>
                       </Link>
-                      <button className="btn bg-primary text-light rounded-pill" title="Not available">Verifica</button>
+                      <button
+                        className="btn bg-primary text-light rounded-pill"
+                        title="Not available"
+                      >
+                        Verifica
+                      </button>
                     </div>
                   </li>
-                  <li><hr className="dropdown-divider" /></li>
-                  <li><h6 className="dropdown-header">Account</h6></li>
-                  <li><a className="dropdown-item sale" href="#" title="Not available">50% di sconto su Sales Nav</a></li>
-                  <li><a className="dropdown-item" href="#" title="Not available">Impostazioni e privacy</a></li>
-                  <li><a className="dropdown-item" href="#" title="Not available">Guida</a></li>
-                  <li><a className="dropdown-item" href="#" title="Not available">Lingua</a></li>
-                  <li><hr className="dropdown-divider" /></li>
-                  <li><h6 className="dropdown-header">Gestisci</h6></li>
-                  <li><a className="dropdown-item" href="#" title="Not available">Post e attività</a></li>
-                  <li><a className="dropdown-item" href="#" title="Not available">Account per la pubblicazione di off...</a></li>
-                  <li><hr className="dropdown-divider" /></li>
-                  <li><a className="dropdown-item" href="#" title="Not available">Esci</a></li>
+                  <li>
+                    <hr className="dropdown-divider" />
+                  </li>
+                  <li>
+                    <h6 className="dropdown-header">Account</h6>
+                  </li>
+                  <li>
+                    <a
+                      className="dropdown-item sale"
+                      href="#"
+                      title="Not available"
+                    >
+                      50% di sconto su Sales Nav
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#" title="Not available">
+                      Impostazioni e privacy
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#" title="Not available">
+                      Guida
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#" title="Not available">
+                      Lingua
+                    </a>
+                  </li>
+                  <li>
+                    <hr className="dropdown-divider" />
+                  </li>
+                  <li>
+                    <h6 className="dropdown-header">Gestisci</h6>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#" title="Not available">
+                      Post e attività
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#" title="Not available">
+                      Account per la pubblicazione di off...
+                    </a>
+                  </li>
+                  <li>
+                    <hr className="dropdown-divider" />
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#" title="Not available">
+                      Esci
+                    </a>
+                  </li>
                 </ul>
               </div>
             </Col>
@@ -137,34 +262,56 @@ export default function CustomNavbar() {
                 <ul
                   className={
                     addFlex
-                      ? "dropdown-menu d-flex aziende dropdown-menu-end justify-content-between"
-                      : "dropdown-menu aziende dropdown-menu-end justify-content-between"
+                      ? 'dropdown-menu d-flex aziende dropdown-menu-end justify-content-between'
+                      : 'dropdown-menu aziende dropdown-menu-end justify-content-between'
                   }
                 >
                   <li className="app px-5 py-4">
                     <h6 className="dropdown-header">Le mie app</h6>
-                    <a className="dropdown-item" href="#" title="Not available"><i className="bi bi-compass"></i> Trova lead</a>
-                    <a className="dropdown-item" href="#" title="Not available"><i className="bi bi-chat-dots"></i> Gruppi</a>
-                    <a className="dropdown-item" href="#" title="Not available"><i className="bi bi-receipt"></i> Gestisci fatturazione</a>
+                    <a className="dropdown-item" href="#" title="Not available">
+                      <i className="bi bi-compass"></i> Trova lead
+                    </a>
+                    <a className="dropdown-item" href="#" title="Not available">
+                      <i className="bi bi-chat-dots"></i> Gruppi
+                    </a>
+                    <a className="dropdown-item" href="#" title="Not available">
+                      <i className="bi bi-receipt"></i> Gestisci fatturazione
+                    </a>
 
                     <h6 className="dropdown-header">Talent</h6>
-                    <a className="dropdown-item" href="#" title="Not available"><i className="bi bi-graph-up"></i> Talents insight</a>
-                    <a className="dropdown-item" href="#" title="Not available"><i className="bi bi-broadcast"></i> Pubblica un'offerta di lavoro</a>
+                    <a className="dropdown-item" href="#" title="Not available">
+                      <i className="bi bi-graph-up"></i> Talents insight
+                    </a>
+                    <a className="dropdown-item" href="#" title="Not available">
+                      <i className="bi bi-broadcast"></i> Pubblica un'offerta di
+                      lavoro
+                    </a>
 
                     <h6 className="dropdown-header">Vendite</h6>
-                    <a className="dropdown-item" href="#" title="Not available"><i className="bi bi-check"></i> Marketplace dei servizi</a>
+                    <a className="dropdown-item" href="#" title="Not available">
+                      <i className="bi bi-check"></i> Marketplace dei servizi
+                    </a>
 
                     <h6 className="dropdown-header">Marketing</h6>
-                    <a className="dropdown-item" href="#" title="Not available"><i className="bi bi-bullseye"></i> Pubblicizza</a>
+                    <a className="dropdown-item" href="#" title="Not available">
+                      <i className="bi bi-bullseye"></i> Pubblicizza
+                    </a>
 
                     <h6 className="dropdown-header">Learning</h6>
-                    <a className="dropdown-item" href="#" title="Not available"><i className="bi bi-film"></i> Learning</a>
+                    <a className="dropdown-item" href="#" title="Not available">
+                      <i className="bi bi-film"></i> Learning
+                    </a>
                   </li>
 
-                  <li className="border-end border mx-2" style={{ height: "85vh" }}></li>
+                  <li
+                    className="border-end border mx-2"
+                    style={{ height: '85vh' }}
+                  ></li>
 
                   <li className="business px-5 py-4">
-                    <h6 className="dropdown-header">Scopri altro per il business</h6>
+                    <h6 className="dropdown-header">
+                      Scopri altro per il business
+                    </h6>
                     <a className="dropdown-item" href="#" title="Not available">
                       <span>Assumi su LinkedIn</span>
                       <br /> Trova, attrai e assumi
@@ -193,7 +340,9 @@ export default function CustomNavbar() {
                       <span>Centro per amministratori</span>
                       <br /> Gestisci i dettagli di fatturazione e account
                     </a>
-                    <a className="dropdown-item" href="#" title="Not available">Crea una pagina aziendale</a>
+                    <a className="dropdown-item" href="#" title="Not available">
+                      Crea una pagina aziendale
+                    </a>
                   </li>
                 </ul>
               </div>
@@ -201,7 +350,10 @@ export default function CustomNavbar() {
 
             {/* Premium */}
             <Col xs={4} md="auto" className="d-none d-md-block">
-              <Nav.Link href="/premium" className="d-flex flex-column align-items-center">
+              <Nav.Link
+                href="/premium"
+                className="d-flex flex-column align-items-center"
+              >
                 <StarFill size={22} />
                 <span className="small">Premium</span>
               </Nav.Link>
@@ -210,15 +362,21 @@ export default function CustomNavbar() {
             <Col xs={4} className="d-block d-sm-none text-center">
               <PersonCircle size={24} />
               <NavDropdown id="nav-dropdown-dark-example" title="" align="end">
-                <NavDropdown.Item href="#" title="Not available">Account</NavDropdown.Item>
-                <NavDropdown.Item href="#" title="Not available">Impostazioni</NavDropdown.Item>
+                <NavDropdown.Item href="#" title="Not available">
+                  Account
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#" title="Not available">
+                  Impostazioni
+                </NavDropdown.Item>
                 <NavDropdown.Divider />
-                <NavDropdown.Item href="#" title="Not available">Esci</NavDropdown.Item>
+                <NavDropdown.Item href="#" title="Not available">
+                  Esci
+                </NavDropdown.Item>
               </NavDropdown>
             </Col>
           </Row>
         </Navbar.Collapse>
       </Container>
     </Navbar>
-  );
+  )
 }
