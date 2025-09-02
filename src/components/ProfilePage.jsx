559fef33ded5508700e1cf28c54d4dfdb5193424
KevR98 <ramil.kevin98@gmail.com>
2025-09-02 16:44:04 +0200

    fixed

--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -1,20 +1,20 @@
-import { useEffect, useState } from "react";
-import Main from "../components/Main";
-import SidebarProfilo from "../components/SidebarProfilo";
-import { Row, Col } from "react-bootstrap";
-import MiniHero from "../components/MiniHero";
-import FooterProfile from "../components/Footer/FooterProfile";
-import { useDispatch } from "react-redux";
-import { SAVE_ME_INFO, saveMeInfoAction } from "../redux/actions";
+import { useEffect, useState } from 'react';
+import Main from '../components/Main';
+import SidebarProfilo from '../components/SidebarProfilo';
+import { Row, Col } from 'react-bootstrap';
+import MiniHero from '../components/MiniHero';
+import FooterProfile from './MyFooter';
+import { useDispatch } from 'react-redux';
+import { SAVE_ME_INFO, saveMeInfoAction } from '../redux/actions';
 
 const ProfilePage = () => {
   const [showMiniHero, setShow] = useState(false);
   const bearer =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2OGI1YTFkOTE2MjdjNjAwMTVmOGM1NmMiLCJpYXQiOjE3NTY3MzM5MTMsImV4cCI6MTc1Nzk0MzUxM30.SOLseepU4Ysb0KnFQYR3yWP1jikhGc89-HCynCKAhuY";
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2OGI1YTFkOTE2MjdjNjAwMTVmOGM1NmMiLCJpYXQiOjE3NTY3MzM5MTMsImV4cCI6MTc1Nzk0MzUxM30.SOLseepU4Ysb0KnFQYR3yWP1jikhGc89-HCynCKAhuY';
 
   const dispatch = useDispatch();
 
-  fetch("https://striveschool-api.herokuapp.com/api/profile/me", {
+  fetch('https://striveschool-api.herokuapp.com/api/profile/me', {
     headers: {
       Authorization: `Bearer ${bearer}`,
     },
@@ -23,20 +23,20 @@ const ProfilePage = () => {
       if (data.ok) {
         return data.json();
       } else {
-        throw new Error("Erroreeee");
+        throw new Error('Erroreeee');
       }
     })
     .then((data) => {
-      console.log("data", data);
+      console.log('data', data);
       dispatch({ type: SAVE_ME_INFO, payload: data });
     })
-    .catch((err) => console.log("Errore!", err));
+    .catch((err) => console.log('Errore!', err));
 
   // useEffect(() => {
   //   dispatch(saveMeInfoAction(bearer));
   // }, []);
 
-  window.addEventListener("scroll", () => {
+  window.addEventListener('scroll', () => {
     const positionY = window.scrollY;
     if (positionY > 300) {
       setShow(true);
