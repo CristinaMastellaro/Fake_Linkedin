dd5b69aca280faeb7d0181fb64b19cb359051555
PernaGiuseppe <giuseppeperna97@hotmail.com>
2025-09-02 15:17:08 +0200

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,9 @@
 import App from './App.jsx'
-import CustomNavbar from './components/Navbar.jsx'
-import NotFound from './components/NotFound.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import './index.css'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 const rootElement = document.getElementById('root')
 const root = createRoot(rootElement)
