f6f9a73b60c9b020ac61ee7efbd1120894f12258
PernaGiuseppe <giuseppeperna97@hotmail.com>
2025-09-05 15:27:25 +0200

    css updates

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
-import App from "./App.jsx";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import "bootstrap-icons/font/bootstrap-icons.css";
-import "./index.css";
-import { createRoot } from "react-dom/client";
-import { Provider } from "react-redux";
-import store from "./redux/store/index.js";
+import App from './App.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import './index.css'
+import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+import store from './redux/store/index.js'
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
+const rootElement = document.getElementById('root')
+const root = createRoot(rootElement)
 
 root.render(
   <Provider store={store}>
@@ -16,4 +16,4 @@ root.render(
       <App />
     </div>
   </Provider>
-);
+)
